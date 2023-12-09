2e1096084bdb95ed2e814b26ef8cdfe5b7aa7e94
EmileLg <e1000.lagarde@gmail.com>
2023-12-09 20:57:34 +0100

    ajouts commentaires prof file_list

--- files-list.c
+++ files-list.c
@@ -6,6 +6,28 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+/*!
+ * @brief clear_files_list clears a files list
+ * @param list is a pointer to the list to be cleared
+ * This function is provided, you don't need to implement nor modify it
+ */
+void clear_files_list(files_list_t *list) {
+    while (list->head) {
+        files_list_entry_t *tmp = list->head;
+        list->head = tmp->next;
+        free(tmp);
+    }
+}
+
+/*!
+ *  @brief add_file_entry adds a new file to the files list.
+ *  It adds the file in an ordered manner (strcmp) and fills its properties
+ *  by calling stat on the file.
+ *  Il the file already exists, it does nothing and returns 0
+ *  @param list the list to add the file entry into
+ *  @param file_path the full path (from the root of the considered tree) of the file
+ *  @return a pointer to the added element if success, NULL else (out of memory)
+ */
 files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
     // Check if the file entry already exists
     files_list_entry_t *existing_entry = find_entry_by_name(list, file_path, 0, 0);
@@ -61,6 +83,14 @@ files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
     return new_entry;
 }
 
+/*!
+ * @brief add_entry_to_tail adds an entry directly to the tail of the list
+ * It supposes that the entries are provided already ordered, e.g. when a lister process sends its list's
+ * elements to the main process.
+ * @param list is a pointer to the list to which to add the element
+ * @param entry is a pointer to the entry to add. The list becomes owner of the entry.
+ * @return 0 in case of success, -1 else
+ */
 int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
     if (!list || !entry) {
         return -1; // Invalid input parameters
@@ -85,6 +115,15 @@ int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
     return 0; // Success
 }
 
+/*!
+ *  @brief find_entry_by_name looks up for a file in a list
+ *  The function uses the ordering of the entries to interrupt its search
+ *  @param list the list to look into
+ *  @param file_path the full path of the file to look for
+ *  @param start_of_src the position of the name of the file in the source directory (removing the source path)
+ *  @param start_of_dest the position of the name of the file in the destination dir (removing the dest path)
+ *  @return a pointer to the element found, NULL if none were found.
+ */
 files_list_entry_t *find_entry_by_name(files_list_t *list, char *file_path, size_t start_of_src, size_t start_of_dest) {
     if (!list || !file_path) {
         return NULL; // Invalid input parameters
@@ -106,6 +145,11 @@ files_list_entry_t *find_entry_by_name(files_list_t *list, char *file_path, size
     return NULL; // Entry not found
 }
 
+/*!
+ * @brief display_files_list displays a files list
+ * @param list is the pointer to the list to be displayed
+ * This function is already provided complete.
+ */
 void display_files_list(files_list_t *list) {
     if (!list) {
         return;
@@ -116,6 +160,11 @@ void display_files_list(files_list_t *list) {
     }
 }
 
+/*!
+ * @brief display_files_list_reversed displays a files list from the end to the beginning
+ * @param list is the pointer to the list to be displayed
+ * This function is already provided complete.
+ */
 void display_files_list_reversed(files_list_t *list) {
     if (!list) {
         return;
@@ -123,13 +172,5 @@ void display_files_list_reversed(files_list_t *list) {
 
     for (files_list_entry_t *cursor = list->tail; cursor != NULL; cursor = cursor->prev) {
         printf("%s\n", cursor->path_and_name);
-    }
-}
-
-void clear_files_list(files_list_t *list) {
-    while (list->head) {
-        files_list_entry_t *tmp = list->head;
-        list->head = tmp->next;
-        free(tmp);
     }
 }
