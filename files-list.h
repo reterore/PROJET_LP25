d64b90794de173ea1fd0f71c4dfb0988c1054ce2
reterore <lucas.vigny@gmail.com>
2023-12-11 20:11:29 +0100

    correction d'une erreur rajoutée dans files-list.h

--- files-list.h
+++ files-list.h
@@ -23,8 +23,6 @@ typedef struct _files_list_entry {
 typedef struct {
     struct _files_list_entry *head;
     struct _files_list_entry *tail;
-    files_list_entry_t *files;
-    size_t size;
 } files_list_t;
 
 void clear_files_list(files_list_t *list);
